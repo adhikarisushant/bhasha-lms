a02ff9410c84e3144c12f859643ce95c6fe6580a
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-09-26 17:23:47 +0545

    user protected and authorization middleware

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -5,11 +5,12 @@ import {
   logoutUser,
   registrationUser,
 } from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
-userRouter.post("/logout", logoutUser);
+userRouter.get("/logout", isAuthenticated, authorizeRoles("admin"), logoutUser);
 
 export default userRouter;

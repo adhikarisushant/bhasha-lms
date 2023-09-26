0b3892661b801611e03ce805d13280fb46ce5f33
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-09-26 12:20:54 +0545

    user registration

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -17,6 +18,9 @@ app.use(
     origin: process.env.ORIGIN,
   })
 );
+
+// routes
+app.use("/api/v1", userRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {

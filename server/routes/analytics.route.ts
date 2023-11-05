214ee066b49e3e312db9ee75e9a5c7fa135ae59b
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-11-05 18:05:14 +0545

    get last 28 days users, orders, courses for last 1 year

---
+++ server/routes/analytics.route.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
+import {
+  getCoursesAnalytics,
+  getOrdersAnalytics,
+  getUserAnalytics,
+} from "../controllers/analytics.controller";
+
+const analyticsRouter = express.Router();
+
+analyticsRouter.get(
+  "/get-users-analytics",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getUserAnalytics
+);
+
+analyticsRouter.get(
+  "/get-courses-analytics",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getCoursesAnalytics
+);
+
+analyticsRouter.get(
+  "/get-orders-analytics",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getOrdersAnalytics
+);
+
+export default analyticsRouter;

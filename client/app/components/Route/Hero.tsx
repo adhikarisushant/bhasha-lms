5594ad2005a9725f69b4317bce3c2cc8fcc1dad0
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-11-22 15:27:15 +0545

    hero component

--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -1,15 +1,28 @@
-import React, { FC } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { BiSearch } from "react-icons/bi";
-
+'use client';
+import React, { FC } from 'react'
+import Link from "next/link"
+import Image from 'next/image'
+import { BiSearch } from 'react-icons/bi'
 
 type Props = {}
 
-const Hero: FC<Props> = (props) => {
+const Hero: FC<Props> = (Props) => {
     return (
-        <div className="w-full 100px:flex items-center">
-            <div className="absolute top-[100px] 100px:top-[unset] 1500px:h-[700px] 1500px:w-[700px]"></div>
+        <div className='w-full h-screen relative flex flex-col 800px:flex-row   items-center justify-between pt-[50px]'>
+            <div className=' 1000px:top-[unset] mx-10 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[600px] 1100px:w-[600px] h-[50vh] w-[50vh] hero_animation rounded-[100%] pt-[50px]'>
+                <div className='w-[100%]  flex h-[100%] items-center justify-center  1000px:pt-0 z-10'>
+                    <Image
+                        src={require("../../../public/assets/banner01.png")}
+                        alt='Vector'
+                        className='object-contain  w-[80%] mb-10 max-w-[85%] h-[auto] z-[10]'
+                    />
+                </div>
+
+            </div>
+            <div className=' 800px:w-[50%] w-[80%] justify-center  flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[1px]'>
+                <h2 className='dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin  1000px:leading-[75px] 1500px:w-[600px]'>Improve Your Online Learning Experience Better Instantly</h2>
+                <p className='dark:text-white text-[#000000c7] text-[12px] px-3 w-full 1000px:text-[15px] font-[400] font-Josefin  1000px:leading-[30px] 1500px:w-[600px]'> We have 40k+ Online Courses & 500k+ Online registerd student.Find your desired Courses from them</p>
+            </div>
         </div>
     )
 }

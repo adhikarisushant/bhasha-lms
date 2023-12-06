18c4573063b8982d03a6b43c23aa7b8c823e3c53
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-12-06 18:09:12 +0545

    course content ui

--- client/app/components/Admin/Course/CreateCourse.tsx
+++ client/app/components/Admin/Course/CreateCourse.tsx
@@ -3,11 +3,12 @@ import React, { useState } from 'react'
 import CourseInformation from "./CourseInformation"
 import CourseOptions from "./CourseOptions"
 import CourseData from "./CourseData"
+import CourseContent from "./CourseContent"
 
 type Props = {}
 
 const CreateCourse = (props: Props) => {
-    const [active, setActive] = useState(0);
+    const [active, setActive] = useState(2);
     const [courseInfo, setCourseInfo] = useState({
         name: "",
         description: "",
@@ -36,6 +37,10 @@ const CreateCourse = (props: Props) => {
 
     const [courseData, setCourseData] = useState({});
 
+    const handleSubmit = async () => {
+
+    }
+
     return (
         <div className="w-full flex min-h-screen">
             <div className="w-[80%]">
@@ -58,6 +63,18 @@ const CreateCourse = (props: Props) => {
                             setPrerequisites={setPrerequisites}
                             active={active}
                             setActive={setActive}
+                        />
+                    )
+                }
+
+                {
+                    active === 2 && (
+                        <CourseContent
+                            active={active}
+                            setActive={setActive}
+                            courseContentData={courseContentData}
+                            setCourseContentData={setCourseContentData}
+                            handleSubmit={handleSubmit}
                         />
                     )
                 }
